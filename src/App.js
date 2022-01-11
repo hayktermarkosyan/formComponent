f7cd4fb5c853a2a649553d93d88e61db2e9c63ea
hayktermarkosyan <hayktermarkosyan@gmail.com>
2022-01-11 20:33:23 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SelectInsideForm from './Components/SelectInsideForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <SelectInsideForm />
+    </div>
+  );
+}
+
+export default App;
